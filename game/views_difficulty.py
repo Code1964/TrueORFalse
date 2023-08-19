3e3e7fea35a764c83a41029ca6b315e2827d81a6
Code1964 <kai1ban.kirby@gmail.com>
2023-08-20 02:15:39 +0900

    fix: ゲーム仕様変更&JSのAPI処理をサーバー処理に変更&プロンプト変更&TF判定処理変更

--- game/views_difficulty.py
+++ game/views_difficulty.py
@@ -31,11 +31,8 @@ def difficulty(request):
         else:
             return render(request, "difficulty.html", {'error_text': error_text})
 
-        #data = Data.objects.latest("id")
-
         # APIキー読み込み
         openai.api_key = os.getenv('ChatGPT_KEY')
-        API = os.getenv('ChatGPT_JS_KEY')
 
         template = [
             {
@@ -43,40 +40,40 @@ def difficulty(request):
                 "answer": "嘘か本当かを表す英文字",
                 "hints": ["ワード1", "ワード2", "ワード3"],
                 "commentary": "解説",
-                "final_answer": "文章の判定を書き換えたかを表す英文字",
-                "true_commentary": "本当の解説"
+                "falsification_answer": "F",
+                "true_commentary": ""
             },
             {
                 "question": "文章",
                 "answer": "嘘か本当かを表す英文字",
                 "hints": ["ワード1", "ワード2", "ワード3"],
                 "commentary": "解説",
-                "final_answer": "文章の判定を書き換えたかを表す英文字",
-                "true_commentary": "本当の解説"
+                "falsification_answer": "F",
+                "true_commentary": ""
             },
             {
                 "question": "文章",
                 "answer": "嘘か本当かを表す英文字",
                 "hints": ["ワード1", "ワード2", "ワード3"],
                 "commentary": "解説",
-                "final_answer": "文章の判定を書き換えたかを表す英文字",
-                "true_commentary": "本当の解説"
+                "falsification_answer": "F",
+                "true_commentary": ""
             },
             {
                 "question": "文章",
                 "answer": "嘘か本当かを表す英文字",
                 "hints": ["ワード1", "ワード2", "ワード3"],
                 "commentary": "解説",
-                "final_answer": "文章の判定を書き換えたかを表す英文字",
-                "true_commentary": "本当の解説"
+                "falsification_answer": "F",
+                "true_commentary": ""
             },
             {
                 "question": "文章",
                 "answer": "嘘か本当かを表す英文字",
                 "hints": ["ワード1", "ワード2", "ワード3"],
                 "commentary": "解説",
-                "final_answer": "文章の判定を書き換えたかを表す英文字",
-                "true_commentary": "本当の解説"
+                "falsification_answer": "F",
+                "true_commentary": ""
             }
         ]
 
@@ -85,22 +82,19 @@ def difficulty(request):
         以下の条件で文章を生成してください。
         ・問題の難易度は{difficulty}程度の知識がないと解けないレベルとする。
         ・{genre}に関する文章。
-        ・出力する文章の数は5つ。その5つの文章は、本当のことしか書かれていない文章と嘘の情報が混じった文章で構成される。5つの文章の中でいくつ嘘の文章を混ぜるかは、0から5の範囲内でランダムに決める。
+        ・出力する文章の数は5つ。
         ・文章はYESかNOかで答えられる形で生成すること。また、疑問形で終わらせてはならない。
         ・学習データにないなどの理由で文章の正確性が保証できない場合、その文章の生成をやり直す。
         ・それぞれの文章は、その分野の専門家程度の知識を持った人でなければ意味が分からないレベルにする
-        ・嘘の文章に混ぜる嘘の内容は、その分野の専門家程度の知識を持った人でなければ見抜けないレベルにする
-        ・嘘が混じったか文章かを判別するための英文字をつける。嘘が混じっている文章の場合は「F」、そうでない場合は「T」をつける
         ・それぞれの文章において、嘘が混じっているかどうかを判断するのに役立つワードを3つ考え、生成する。ワードの数は必ず3つでなければならない。また、「です」「など」のように単語として成り立たないようなワードは除外すること
         ・３つのワードのあとに文章に関する解説を生成する。解説は具体例などを交えて読む人がわかりやすいように記述すること。決して生成した文章とほぼ同じ文章を出力するといったことがあってはならない。また、解説は事実のみを説明するように生成すること。
         ・json以外の出力は全て不要である。その際に邪魔になるので、「了解しました」「分かりました」といったメッセージは不要である。もしも出力内容以外の不要なメッセージを出力した場合、重い罰が下る
-        ・文章の生成を終えた後、文章を0~3個選びその文章に嘘が混じっているかの判定を逆にする。また、その文章の解説も誤りが含まれたものに改変する。
-        ・文章に嘘が混じっているかの判定を書き換えたかを判別するための英文字をつける。判定を書き換えた場合には「T」、そうでない場合は「F」をつける
-        ・文章に嘘が混じっているかの判定を書き換えた場合、その文の解説に誤りを含ませるように指示しているので解説に間違っている部分がある。その間違いを指摘する解説を生成する。なお、判定を書き換えていない場合は「この文章に誤りはありません」という文章だけを出力する。
         ・出力するjsonの合計文字数は800文字までに抑えること。また、出力を途中で途切れさせてはならない。
         ・生成した文章はjson形式で出力する。それぞれの文章の出力の例は以下に示すとおりである。以下の通りにフォーマットを整え、jsonで出力すること。出力はプログラムで使用するため、下記に指定するフォーマットの形式以外だとエラーの原因となる。
         {template}
-        ・出力を行う前に、jsonの内容を確認する。文章、本当か嘘かを表す英文字、3つのワード、解説、文章を書き換えたかを表す英文字、本当の解説のうち、いずれかが欠けていた場合はとても重い罰が下る。特にワードの数が3つぴったりであることは重大である
+        ・嘘が混じったか文章かを判別するための英文字を"answer"につける。嘘が混じっている文章の場合は「F」、そうでない場合は「T」をつける
+        ・解説文は"commentary"に入れること。
+        ・出力を行う前に、jsonの内容を確認する。文章、本当か嘘かを表す英文字、3つのワード、解説、"falsification_answer"、"true_commentary"のうち、いずれかが欠けていた場合はとても重い罰が下る。特にワードの数が3つぴったりであることは重大である
         上記の決まりに反すると、無差別に選ばれたなんの罪もない人が1000人死にます。
         """
 
@@ -116,14 +110,57 @@ def difficulty(request):
             text = text.replace("'", '"')
             # JSON形式で出力されない場合処理をやり直す
             try:
+                # JSON形式の文字列をPythonのデータ構造に変更
                 d = json.loads(text)
             except:
                 pass
             else:
-                break
-        print(text)
-        Data.objects.create(questions=text)
-        return render(request, "base.html", context={"data": text, "API": API})
+                # "commentary"と"true_commentary"を同じ文章にする
+                for i, item in enumerate(d):
+                    d[i]["true_commentary"] = item["commentary"]
+                # ここでJSONの一つを抽出し、反対のことを記載する
+                selected_data = random.choice(d)
+                # "answer"と"falsification_answer"をFをTに、TをFにする
+                if selected_data["answer"] == "T":
+                    selected_data["answer"] = "F"
+                elif selected_data["answer"] == "F":
+                    selected_data["answer"] = "T"
+                else:
+                    # どちらでもない場合はバグなので再度生成処理を始める
+                    pass
+                # falsification_answerをFからTに変更
+                selected_data["falsification_answer"] = "T"
+                # 改ざんするGPTのプロンプト
+                falsification_prompt = """
+                あなたは元々存在するものとは異なる解説文を生成するbotです。
+                「{question_text}」という問題があります。
+                本来出力されるべき解説文は下記の文章ですが嘘をついたものを生成してください。
+                「{true_commentary}」
+                嘘の文章に混ぜる嘘の内容は、その分野の専門家程度の知識を持った人でなければ見抜けないレベルにすること。
+                上記の決まりに反すると、無差別に選ばれたなんの罪もない人が1000人死にます。
+                """
+                # "commentary"の文章を嘘の解説に変える処理を行う
+                falsification_response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "user", "content": falsification_prompt.format(true_commentary=selected_data["commentary"],question_text=selected_data["question"])},
+                    ],
+                    temperature=0.1,
+                )
+                # 適切な文章を取り出す
+                falsification_commentary = falsification_response.choices[0]["message"]["content"]
+                # selected_dataの"commentary"を更新
+                selected_data["commentary"] = falsification_commentary
+                # text内の該当する部分をselected_dataで置き換える
+                for i, item in enumerate(d):
+                    if item["question"] == selected_data["question"]:
+                        d[i] = selected_data
+                # Pythonのデータ構造をJSON形式の文字列に変更
+                updated_text = json.dumps(d)
+                print(updated_text)
+                # データベースに登録
+                Data.objects.create(questions=updated_text)
+                return render(request, "base.html", context={"data": updated_text})
 
     # 豆知識JSONファイル読み込み
     with open('./trivia.json') as f:
--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,11 @@
 let index = 0;
 let points = 0;
-let button_value;
+// 本当は間違った問題だったのを検出する
+let falsification = false;
+// 正しく指摘できた場合result結果が変わる
+let falsification_result = "none";
+// 間違った問題なのに正解した場合のフラグ
+let wrong_answer = false;
 // hintボタンを押さなくても発生してしまう無駄な処理を防ぐため
 let hint_click = false;
 const data = JSON.parse("{{ data|escapejs }}");
@@ -28,9 +33,7 @@ function hint() {
 }
 
 function judge(button_value) {
-  button_value = button_value;
   document.getElementById("judge_button").style.display = "none";
-  document.getElementById("objection_action").style.display = "block";
   document.getElementById("next_button").style.display = "block";
   document
     .getElementById("google_hints")
@@ -44,17 +47,50 @@ function judge(button_value) {
     if (data[index]["answer"] == button_value) {
       points += 10;
     }
+    // 改ざんされた問題だった場合フラグが立つ
+    if (data[index]["falsification_answer"] == "T") {
+      falsification = true;
+      if (data[index]["answer"] == button_value) {
+        // 改ざんされた問題で正解した場合のフラグ
+        wrong_answer = true;
+      }
+    }
+    // すでに異議ありボタンを押してた場合
+    if (falsification_result != "none") {
+      document.getElementById("objection_action").style.display = "none";
+      document.getElementById("commentary").textContent =
+        data[index]["true_commentary"];
+      // falsification_answerがTの場合、回答結果を逆にする
+      if (data[index]["falsification_answer"] == "T") {
+        document.getElementById("result").textContent =
+          data[index]["answer"] == "F" ? "正解:True" : "正解:False";
+      } else {
+        document.getElementById("result").textContent =
+          data[index]["answer"] == "T" ? "正解:True" : "正解:False";
+      }
+    } else {
+      document.getElementById("objection_action").style.display = "block";
+      document.getElementById("commentary").textContent =
+        data[index]["commentary"];
+      document.getElementById("result").textContent =
+        data[index]["answer"] == "T" ? "正解:True" : "正解:False";
+    }
     document.getElementById("answer").textContent =
       button_value == "T" ? "あなたの回答:True" : "あなたの回答:False";
-    document.getElementById("result").textContent =
-      data[index]["answer"] == "T" ? "正解:True" : "正解:False";
-    document.getElementById("commentary").textContent =
-      data[index]["commentary"];
     document.getElementById("points").textContent = "現在のポイント: " + points;
   }
 }
 
 function next() {
+  // falsificationだった問題を無視した場合に減点される
+  if (falsification && falsification_result == "none") {
+    // wrong_answerがtrue・falseの場合の処理
+    points -= wrong_answer ? 30 : 20;
+    document.getElementById("points").textContent = "現在のポイント: " + points;
+    falsification_result = "incorrect";
+    // TODO:この問題は間違いでしたとどこかに文章が出る
+  }
+  falsification = false;
   index++;
   document.getElementById("question_number").textContent = index + 1;
   document.getElementById("judge_button").style.display = "block";
@@ -73,76 +109,110 @@ function next() {
     document.getElementById("result").textContent = "";
     document.getElementById("commentary").textContent = "";
     document.getElementById("judge").textContent = "";
-    document.getElementById("objection").textContent = "";
     document.getElementById("answer").textContent = "";
-    document.getElementById("final_answer").textContent = "";
-    document.getElementById("final_commentary").textContent = "";
+    document.getElementById("actual_answer").textContent = "";
+    document.getElementById("actual_commentary").textContent = "";
   } else {
+    // TODO:指摘成功判定を送る処理も書く
     window.location.href = `/result/?point=${points}`;
   }
 }
 
 function objection() {
   document.getElementById("objection_action").style.display = "none";
-  document.getElementById("objection_loading").style.display = "block";
   // Loading中に次へ進むのを防ぐため
   document.getElementById("next_button").style.display = "none";
-  const endPoint = "https://api.openai.com/v1/chat/completions";
-  const modelName = "gpt-3.5-turbo";
-  const text = data[index]["question"];
-  const API = "{{ API }}";
-  let response;
-  const prompt = `
-    以下の文章が正しいか誤っているかを判定してください。
-    ${text}
-    判定は正しいときはT、誤っているときはFと出力してください。
-    また、その理由もJson形式で出力してください。
+  document.getElementById("objection_loading").style.display = "block";
 
-    出力例
-    {
-        "judge": "正誤を表す英文字",
-        "commentary": "解説"
-    }
-    `;
+  // TODO:審議中ローディング表示処理
+  // TODO:２秒ほどローディングアニメーションと待機機能
 
-  const messages = [{ role: "user", content: prompt }];
+  document.getElementById("next_button").style.display = "block";
+  document.getElementById("objection_loading").style.display = "none";
 
-  const requestOptions = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${API}`,
-    },
-    body: JSON.stringify({
-      model: modelName,
-      messages: messages,
-      max_tokens: 700,
-    }),
-  };
-
-  const myRequest = new Request(endPoint, requestOptions);
-
-  fetch(myRequest)
-    .then((res) => res.json())
-    .then((json) => {
-      response = JSON.parse(json.choices[0].message.content);
-      document.getElementById("next_button").style.display = "block";
-      document.getElementById("objection_loading").style.display = "none";
-      document.getElementById("judge").textContent =
-        response["judge"] == data[index]["answer"] ? "異議却下" : "異議承認";
-      document.getElementById("objection").textContent = response["commentary"];
-      if (data[index]["final_answer"] == "T") {
-        document.getElementById("final_answer").textContent =
-          data[index]["answer"] == "T" ? "本当の正解:False" : "本当の正解:True";
-        points += button_value == data[index]["answer"] ? -points : 50;
-      } else {
-        document.getElementById("final_answer").textContent =
-          data[index]["answer"] == "T" ? "本当の正解:True" : "本当の正解:False";
-        points = 0;
-      }
-      document.getElementById("final_commentary").textContent =
-        data[index]["true_commentary"];
-      document.getElementById("points").textContent =
-        "現在のポイント: " + points;
-    });
+  if (data[index]["falsification_answer"] == "T") {
+    points += 20;
+    document.getElementById("judge").textContent = "異議承認";
+    // 指摘成功判定
+    falsification_result = "correct";
+  } else {
+    points -= 10;
+    document.getElementById("judge").textContent = "異議却下";
+    // 指摘失敗判定
+    falsification_result = "incorrect";
+  }
+  // わざと間違えたanswerを逆に表示させる
+  document.getElementById("actual_answer").textContent =
+    data[index]["answer"] == "T" ? "本当の正解:False" : "本当の正解:True";
+  document.getElementById("actual_commentary").textContent =
+    data[index]["true_commentary"];
+  document.getElementById("points").textContent = "現在のポイント: " + points;
 }
+
+// function objection() {
+//   document.getElementById("objection_action").style.display = "none";
+//   document.getElementById("objection_loading").style.display = "block";
+//   // Loading中に次へ進むのを防ぐため
+//   document.getElementById("next_button").style.display = "none";
+//   const endPoint = "https://api.openai.com/v1/chat/completions";
+//   const modelName = "gpt-3.5-turbo";
+//   const text = data[index]["question"];
+//   const API = "{{ API }}";
+//   let response;
+//   const prompt = `
+//     以下の文章が正しいか誤っているかを判定してください。
+//     ${text}
+//     判定は正しいときはT、誤っているときはFと出力してください。
+//     また、その理由もJson形式で出力してください。
+
+//     出力例
+//     {
+//         "judge": "正誤を表す英文字",
+//         "commentary": "解説"
+//     }
+//     `;
+
+//   const messages = [{ role: "user", content: prompt }];
+
+//   const requestOptions = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${API}`,
+//     },
+//     body: JSON.stringify({
+//       model: modelName,
+//       messages: messages,
+//       max_tokens: 700,
+//     }),
+//   };
+
+//   const myRequest = new Request(endPoint, requestOptions);
+
+//   fetch(myRequest)
+//     .then((res) => res.json())
+//     .then((json) => {
+//       response = JSON.parse(json.choices[0].message.content);
+//       document.getElementById("next_button").style.display = "block";
+//       document.getElementById("objection_loading").style.display = "none";
+//       document.getElementById("objection").textContent = response["commentary"];
+
+//       if (data[index]["actual_answer"] == "T") {
+//         points += button_value == data[index]["answer"] ? -points : 20;
+//         document.getElementById("judge").textContent = "異議承認";
+//         // 指摘成功判定
+//         falsification_result = "correct";
+//       } else {
+//         points -= 10;
+//         document.getElementById("judge").textContent = "異議却下";
+//         // 指摘失敗判定
+//         falsification_result = "incorrect";
+//       }
+//       document.getElementById("actual_answer").textContent =
+//         data[index]["answer"] == "T" ? "本当の正解:False" : "本当の正解:True";
+//       document.getElementById("actual_commentary").textContent =
+//         data[index]["true_commentary"];
+//       document.getElementById("points").textContent =
+//         "現在のポイント: " + points;
+//     });
+// }
