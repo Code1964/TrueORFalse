5ac511c2daed7f1639c2e1c7f1094b6a78058361
Code Alive <50915173+Code1964@users.noreply.github.com>
2023-08-24 23:05:13 +0900

    add: 指定された問題のデータベース保存処理(未完成) (#8)
    * add: 指定された問題のデータベース保存処理
    
    * fix: 問題のデータベース保存処理

--- game/models.py
+++ game/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Data(models.Model):
-    questions = models.JSONField()
-    #データを追加した時間
-    created_at = models.DateTimeField(auto_now_add=True)
+    question = models.TextField(max_length=200)
+    answer = models.CharField(max_length=1)
+    hints = models.JSONField()
+    commentary = models.TextField(max_length=200)
+    falsification_answer = models.CharField(max_length=1)
+    true_commentary = models.TextField(max_length=200)
+    genre = models.TextField(max_length=30)
+    # 生成問題が正しいか否か判断
+    is_objection = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question
