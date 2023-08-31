2e5d0fe70772c76e54edef8d08dcee827fc2e5ac
Code Alive <50915173+Code1964@users.noreply.github.com>
2023-09-01 02:32:31 +0900

    add: 精度の低い問題を晒すページ (#18)

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from . models import Data
+from . models import Data, Comment
 
 # Register your models here.
 # 管理ツールに登録
 admin.site.register(Data)
+admin.site.register(Comment)
