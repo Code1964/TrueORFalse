54bbe8a7962a636aa75f7583d478f8cdf96ca507
Code1964 <kai1ban.kirby@gmail.com>
2023-09-03 15:52:06 +0900

    add: 最初のページ追加

--- game/urls.py
+++ game/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from . import views, views_difficulty, views_problem
+from . import views_home, views_difficulty, views_problem, views_result
 
 app_name = 'game'
 urlpatterns = [
+    path("", views_home.home, name="home"),
     path("existing_difficulty/", views_difficulty.existing_difficulty, name="existing_difficulty"),
     path("difficulty/", views_difficulty.difficulty, name="difficulty"),
-    path("result/", views.result, name="result"),
-    path('result/saveDB', views.saveDB, name='saveDB'),
+    path("result/", views_result.result, name="result"),
+    path('result/saveDB', views_result.saveDB, name='saveDB'),
     path("problems/", views_problem.problems, name="problems"),
     path('problem/<int:problem_id>/', views_problem.problem_thread, name='problem_thread'),
     path('problem/<int:problem_id>/add_comment/', views_problem.add_comment, name='add_comment'),
